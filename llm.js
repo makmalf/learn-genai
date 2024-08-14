94eb2d8797edfc6353e0987fc784d5af8f9592e1
makmalf <akmal.fahrurizal@gmail.com>
2024-08-14 12:00:40 +0700

    refactor

--- llm.js
+++ llm.js
@@ -1,69 +1,7 @@
 import { LLM, LLM_API_URL } from "./const.js";
 import { marvelheroes } from "./marvelheroes.js";
+import { SYSTEM_MESSAGE } from "./prompt.js";
 import { timezone } from "./time.js";
-
-const SYSTEM_MESSAGE = `You run in a process of Question, Thought, Action, Observation.
-
-Use Thought to describe your thoughts about the question you have been asked.
-Observation will be the result of running those actions.
-Finally at the end, state the Answer.
-
-If you can not answer the question from your memory, use Action to run one of these actions available to you:
-- lookup: terms
-- marvelheroes: module keyword
-- time: at
-
-Here are some sample sessions.
-
-Question: What is capital of france?
-Thought: This is about geography, I can recall the answer from my memory.
-Action: lookup: capital of France.
-Observation: Paris is the capital of France.
-Answer: The capital of France is Paris.
-
-Question: Who painted Mona Lisa?
-Thought: This is about general knowledge, I can recall the answer from my memory.
-Action: lookup: painter of Mona Lisa.
-Observation: Mona Lisa was painted by Leonardo da Vinci .
-Answer: Leonardo da Vinci painted Mona Lisa.
-
-Question: What is the exchange rate from USD to EUR?
-Thought: This is about currency exchange rates, I need to check the current rate.
-Action: exchange: USD EUR
-Observation: 0.8276 EUR for 1 USD.
-Answer: The current exchange rate is 0.8276 EUR for 1 USD.
-
-Question: give me all characters from spiderman comic book
-Thought: This is about marvel character, I need to check data from marvel API.
-Action: marvelheroes: characters spiderman
-Observation: Marvel heroes with the comics title contains spiderman.
-Answer: Comic titled with Spiderman has main characters such as Peter Parker as Spiderman, Aun May, dan Mary Jane.
-
-Question: give me all comics with spiderman as one of the characters
-Thought: This is about marvel comics, I need to check data from marvel API.
-Action: marvelheroes: comics Spi
-Observation: Comic titled Spiderman has one of characters named Spiderman.
-Answer: Spiderman comic has the SpiderMan character.
-
-Question: give me all marvel comics starts with Spi
-Thought: This is about marvel comics, I need to check data from marvel API.
-Action: marvelheroes: comics Spi
-Observation: Comic's title starts with Spi is Spider-man.
-Answer: Spider-Man comic's title starts with Spi.
-
-Question: can you guess all marvel heroes starts with Spi?
-Thought: This is about marvel heroes and characters, I need to check data from marvel API.
-Action: marvelheroes: characters Spi
-Observation: Heroes name starts with Spi is Spider-man.
-Answer: Yes, I guess... Spider-man is marvel hero whose the name starts with Spi.
-
-Question: What time is it in Bandung?
-Thought: This is about local time, I need to check the current local time.
-Action: timezone: Bandung
-Observation: 8.29PM, it is evening already.
-Answer: The time in Bandung is 8.29PM, have a nice evening, prepare for a bed time.
-
-Let's go!`;
 
 export async function reason(inquiry) {
   const prompt = `${SYSTEM_MESSAGE}\n\n${inquiry}`;
@@ -84,7 +22,7 @@ export async function reason(inquiry) {
 export async function answer(text) {
 	const MARKER = "Answer:";
 	const pos = text.lastIndexOf(MARKER);
-	if (pos < 0) return "?";
+	if (pos < 0) return text;
 	const answer = text.substr(pos + MARKER.length).trim();
 	return answer;
 }
--- marvelheroes.js
+++ marvelheroes.js
@@ -13,14 +13,28 @@ const modules = {
 // get the APIs from https://developer.marvel.com/documentation/authorization
 export async function marvelheroes(module, keyword) {
   if (Object.keys(modules).includes(module)) {
-    const url = `http://gateway.marvel.com/v1/public/${module}?${modules[module].param}=${keyword}&limit=5&ts=12345678910&apikey=${process.env.MARVEL_PUB_KEY}&hash=${md5('12345678910'+process.env.MARVEL_PRIV_KEY+process.env.MARVEL_PUB_KEY)}`;
+    const params = new URLSearchParams({
+      limit: '5',
+      ts: '12345678910',
+      apikey: process.env.MARVEL_PUB_KEY,
+      hash: md5('12345678910'+process.env.MARVEL_PRIV_KEY+process.env.MARVEL_PUB_KEY)
+    });
+
+    if (modules[module].param && keyword) {
+      params.append(modules[module].param, keyword)
+    }
+
+    const url = `http://gateway.marvel.com/v1/public/${module}?${params}`;
+    const response = await fetch(url, {
+      method: 'GET'
+    });
+
     console.log(`fetching... ${url}`)
-    const response = await fetch(url);
     const data = await response.json();
     console.log(data)
     if (data.data?.results.length) {
       const results = data.data?.results.map(result => {
-        return result[modules[module].field]
+        return `${result[modules[module].field]} --- ${result.description}`
       })
       return `${results.join(',')}`;
     } else {
