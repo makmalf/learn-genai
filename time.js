7d308c3acdfaab15fea59a7e3ac7a4eb16e67127
makmalf <akmal.fahrurizal@gmail.com>
2024-08-16 23:54:44 +0700

    support multiple timezone

--- time.js
+++ time.js
@@ -1,10 +1,13 @@
-import moment from "moment";
+import moment from "moment-timezone";
 
-export function timezone(zone) {
-  const zones = ['jakarta', 'bandung', 'jogjakarta', 'surabaya']
-  if (zones.includes(zone.toLowerCase())) {
-    const now = moment()
+export function timezone(zone, zoneCode) {
+  console.log({
+    zone: zone,
+    code: zoneCode
+  })
+  if (zoneCode) {
+    const now = `${moment().tz(zoneCode).format('HH:mm')} in ${zone}, I have to give some recommendations about activities to do during this time in this city`
     return now;
   }
-  return null
+  return null;
 }
